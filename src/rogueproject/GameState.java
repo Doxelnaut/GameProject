acd1e296bd57281c95e6105aef4deeed970b19a9
doxelnaut <doxelnaut@localhost>
2014-12-05 10:57:36 -0800

    Manually merged Ryans latest Push into my code.

---
+++ src/rogueproject/GameState.java
@@ -0,0 +1,39 @@
+package rogueproject;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class GameState implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public static final int WARRIOR = 0;
+
+	Player player;
+	Player player2;
+	
+	boolean[][] blocked;
+	boolean[][] occupied; // for collision detection with actors
+	NodeMap pathmap;
+	public static Minotaur minotaur;
+	ArrayList<IsoEntity> actors;
+	public static ArrayList<IsoEntity> ground;
+	public static ArrayList<IsoEntity> blocks;
+	public static ArrayList<IsoEntity> walls;
+	public static ArrayList<IsoEntity> wallsandblocks;
+	public static ArrayList<IsoEntity> stop;
+	public static Fireball fireball;
+	
+	GameState(){
+		player = null;
+		player2 = null;
+		ground = new ArrayList<IsoEntity>(100);
+		blocks = new ArrayList<IsoEntity>(100);
+		walls = new ArrayList<IsoEntity>(100);
+		wallsandblocks = new ArrayList<IsoEntity>(200);
+		stop = new ArrayList<IsoEntity>(100);
+		return;
+	}
+}
