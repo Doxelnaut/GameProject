902aa33fecabde6f8f5dde1e8b4fb15fc1771348
Ryan RyanBergquist Bergquist <r.bergquist7@gmail.com>
2014-12-05 16:42:22 -0800

    Changed some files

--- src/rogueproject/Ground.java
+++ src/rogueproject/Ground.java
@@ -7,9 +7,9 @@ import org.newdawn.slick.Image;
 
 public class Ground extends IsoEntity {
 	public Ground(Vector wWorldSize, Vector wPosition) {
-		super(wWorldSize,IsoWorldGame.tileSize);
+		super(wWorldSize,RogueGame.TILE_SIZE);
 		
-		Image isoImg = ResourceManager.getSpriteSheet(IsoWorldGame.groundSheetPath, 64, 32).getSprite(0,0); 
+		Image isoImg = ResourceManager.getSpriteSheet(RogueGame.groundSheetPath, 64, 32).getSprite(0,0); 
 		addImage(isoImg);
 		setZHeightFromIsoImage(isoImg);
 		setPosition(wPosition);
